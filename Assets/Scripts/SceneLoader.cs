c41f2f76485428a7d795e6aecd2a969d6c823a44
Max <61809127+maxwellpark@users.noreply.github.com>
2022-07-17 19:17:09 +0100

    SceneLoader logic

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,38 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public string tutorialSceneName;
+    public string titleSceneName;
+    public string arenaSceneName;
 
     public float nextSceneCountdown;
     public int currentScene;
 
-
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            LoadBattleAI("Tutorial Scene");
-            currentScene = 1;
+            var scene = SceneManager.GetActiveScene();
+            if (scene.name == tutorialSceneName)
+                SceneManager.LoadScene(titleSceneName);
+            else if (scene.name == titleSceneName)
+                SceneManager.LoadScene(arenaSceneName);
         }
 
-        DontDestroyOnLoad(this.gameObject); 
+        //if(Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    LoadBattleAI("Tutorial Scene");
+        //    currentScene = 1;
+        //}
 
 
         
-        if(Input.GetKeyUp(KeyCode.Space)&& currentScene==1)
-        {
-            LoadBattleAI("ReloadingTestScene");
+        //if(Input.GetKeyUp(KeyCode.Space)&& currentScene==1)
+        //{
+        //    LoadBattleAI("ReloadingTestScene");
            
-        }
+        //}
 
 
 
