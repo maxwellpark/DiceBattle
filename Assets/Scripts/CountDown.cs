7e9ae9538e67e24fc914e49370b6210a4516519d
Max <61809127+maxwellpark@users.noreply.github.com>
2022-07-17 13:10:58 +0100

    Fix shots remaining reference in CountDown

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -1,20 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class CountDown : MonoBehaviour
 {
-
-    public float myFinalCountdown=10;
+    public float myFinalCountdown = 10;
     public float myTest;
     public PlayerShooting _playerShooting;
 
     public Text countDisplay;
 
-     public Text countAmunitionPlayer1;
-      public Text countAmunitionPlayer2;
+    public Text countAmunitionPlayer1;
+    public Text countAmunitionPlayer2;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,17 +20,8 @@ public class CountDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myFinalCountdown-=1f* Time.deltaTime;
-        countDisplay.text= "Time"+ myFinalCountdown;
-
-        countAmunitionPlayer1.text=_playerShooting._shotsRemaining.ToString();
-
-     
-      
-       
-
-       
-        
-        
+        myFinalCountdown -= 1f * Time.deltaTime;
+        countDisplay.text = "Time" + myFinalCountdown;
+        countAmunitionPlayer1.text = _playerShooting.shotsRemaining.ToString();
     }
 }
