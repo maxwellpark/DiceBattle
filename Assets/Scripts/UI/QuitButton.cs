43e17e6fb252d17c536e5fa3ed22d04f7e68d96d
Max <61809127+maxwellpark@users.noreply.github.com>
2022-09-17 10:35:34 +0100

    Add quit button component and update title screen

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button _quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _quitButton = GetComponent<Button>();
+        _quitButton.onClick.RemoveAllListeners();
+        _quitButton.onClick.AddListener(Quit);
+    }
+
+    private void Quit()
+    {
+        Debug.Log("Quitting game...");
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else if (Application.isPlaying)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            throw new NotImplementedException("Quit mode not supported");
+        }
+    }
+}
