d9c309f3b2ef0f98787c48b683522c1f300e9fa8
Max <61809127+maxwellpark@users.noreply.github.com>
2022-09-19 14:27:04 +0100

    Support transition to previous scene and btn control

--- Assets/Scripts/MenuTransitionManager.cs
+++ Assets/Scripts/MenuTransitionManager.cs
@@ -4,20 +4,42 @@ using UnityEngine.SceneManagement;
 public class MenuTransitionManager : Singleton<MenuTransitionManager>
 {
     private MusicManager _musicManager;
+    private Scene _previousScene;
 
     protected override void Awake()
     {
         base.Awake();
+        _previousScene = SceneManager.GetActiveScene();
         _musicManager = FindObjectOfType<MusicManager>();
         DontDestroyOnLoad(gameObject);
     }
 
     public void Transition(MenuTransitionData data)
     {
-        if (!string.IsNullOrWhiteSpace(data.sceneName))
-            SceneManager.LoadScene(data.sceneName);
+        Transition(data.sceneName, data.music);
+    }
 
-        if (data.music != MusicState.None)
-            _musicManager.PlayMusic(data.music);
+    public void Transition(string sceneName, MusicState music)
+    {
+        _previousScene = SceneManager.GetActiveScene();
+
+        if (!string.IsNullOrWhiteSpace(sceneName))
+            SceneManager.LoadScene(sceneName);
+
+        if (music != MusicState.None)
+            _musicManager.PlayMusic(music);
+    }
+
+    public void ToPreviousScene()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.name == _previousScene.name)
+        {
+            Debug.LogWarning("Cannot go to previous scene as current scene matches previous scene");
+            return;
+        }
+        var music = _musicManager.GetStateByScene(_previousScene.name);
+        Transition(_previousScene.name, music);
     }
 }
--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum MusicState
@@ -11,6 +11,10 @@ public class MusicManager : Singleton<MusicManager>
     public AudioSource battleSrc;
     public AudioSource battleEndSrc;
     public AudioSource menuSrc;
+    private AudioSource _currentSrc;
+
+    private Dictionary<MusicState, AudioSource> _srcMap;
+    private Dictionary<string, MusicState> _sceneMap;
 
     protected override void Awake()
     {
@@ -18,35 +22,38 @@ public class MusicManager : Singleton<MusicManager>
         menuSrc.enabled = true;
         battleSrc.enabled = true;
         battleEndSrc.enabled = true;
+
+        _srcMap = new Dictionary<MusicState, AudioSource>
+        {
+            { MusicState.Battle, battleSrc },
+            { MusicState.BattleEnd, battleEndSrc },
+            { MusicState.Menu, menuSrc },
+            { MusicState.None, null }
+        };
+
+        _sceneMap = new Dictionary<string, MusicState>
+        {
+            { "BattleScene", MusicState.Battle },
+            { "BattleEndScene", MusicState.BattleEnd },
+            { "BattleMenuScene", MusicState.Menu },
+            { "TitleScreenScene", MusicState.Menu },
+            { "CharacterSelectScene", MusicState.Menu },
+            { "CreditsScene", MusicState.Menu },
+            { "TutorialScene", MusicState.Menu }
+        };
+        _currentSrc = null;
     }
 
     public void PlayMusic(MusicState state)
     {
-        AudioSource src;
+        var src = GetSrcByState(state);
 
-        switch (state)
-        {
-            case MusicState.Battle:
-                src = battleSrc;
-                break;
-            case MusicState.BattleEnd:
-                src = battleEndSrc;
-                break;
-            case MusicState.Menu:
-                src = menuSrc;
-                break;
-            case MusicState.None:
-                ResetSrcs();
-                return;
-            default:
-                throw new Exception("Invalid music state");
-        }
-
-        if (src.isPlaying)
+        if (src == null || src.isPlaying)
             return;
 
         ResetSrcs();
         src.Play();
+        _currentSrc = src;
     }
 
     private void ResetSrcs()
@@ -54,5 +61,60 @@ public class MusicManager : Singleton<MusicManager>
         battleSrc.Stop();
         battleEndSrc.Stop();
         menuSrc.Stop();
+    }
+
+    private AudioSource GetSrcByState(MusicState state)
+    {
+        if (_srcMap.ContainsKey(state))
+            return _srcMap[state];
+
+        Debug.LogWarning("No item exists in src map with state key: " + state);
+        return null;
+        //return state switch
+        //{
+        //    MusicState.Battle => battleSrc,
+        //    MusicState.BattleEnd => battleEndSrc,
+        //    MusicState.Menu => menuSrc,
+        //    MusicState.None => null,
+        //    _ => throw new Exception("Invalid music state"),
+        //};
+    }
+
+    public MusicState GetStateBySrc(AudioSource src)
+    {
+        foreach (var item in _srcMap)
+        {
+            if (item.Value == src)
+                return item.Key;
+        }
+        Debug.LogWarning("No key found in src map for audio src: " + src);
+        return MusicState.None;
+
+        //if (src == battleSrc)
+        //    return MusicState.Battle;
+
+        //if (src == battleEndSrc)
+        //    return MusicState.BattleEnd;
+
+        //if (src == menuSrc)
+        //    return MusicState.Menu;
+
+        //Debug.LogError("Audio src is either null or nothing is playing");
+        //return MusicState.None;
+    }
+
+    public MusicState GetStateByScene(string sceneName)
+    {
+        if (_sceneMap.ContainsKey(sceneName))
+            return _sceneMap[sceneName];
+
+        Debug.LogWarning("No key found in scene map with key: " + sceneName);
+        return MusicState.None;
+    }
+
+    public MusicState GetCurrentState()
+    {
+        var state = GetStateBySrc(_currentSrc);
+        return state;
     }
 }
