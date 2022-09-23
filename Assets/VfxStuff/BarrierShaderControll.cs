e70f00fb60fa127f15836ad250c93947911c5878
j <72587410+akimkalsky@users.noreply.github.com>
2022-09-23 11:40:50 -0300

    ah forgot sttff

--- Assets/VfxStuff/BarrierShaderControll.cs
+++ Assets/VfxStuff/BarrierShaderControll.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BarrierShaderControll : MonoBehaviour
-{ 
+{
     // Start is called before the first frame update
     public Material mat;
 
     public MeshRenderer myRendere;
     public int barrierState;
 
-    private void Start() 
+    private void Start()
     {
         myRendere.GetComponent<MeshRenderer>();
-        
+
     }
 
     // Update is called once per frame
@@ -21,14 +21,14 @@ public class BarrierShaderControll : MonoBehaviour
     {
 
         //one hit
-        if (barrierState==0) 
-        { 
-            
+        if (barrierState == 0)
+        {
+
             myRendere.material.SetFloat("_Enabledistortion", 0f);
-             myRendere.material.SetFloat("_Globalopacity",0.8f);
-             myRendere.material.SetColor("_Maincolor", Color.cyan);
-            
-             
+            myRendere.material.SetFloat("_Globalopacity", 0.8f);
+            myRendere.material.SetColor("_Maincolor", Color.cyan);
+
+
 
         }
 
@@ -36,33 +36,33 @@ public class BarrierShaderControll : MonoBehaviour
 
 
         //one hit
-        if (barrierState==1) 
+        if (barrierState == 1)
         {
             myRendere.material.SetFloat("_Enabledistortion", 0.2f - mat.GetFloat("_Enabledistortion"));
-             myRendere.material.SetFloat("_Globalopacity",0.7f);
-             myRendere.material.SetFloat("_Distortionspeed",0.5f);
+            myRendere.material.SetFloat("_Globalopacity", 0.7f);
+            myRendere.material.SetFloat("_Distortionspeed", 0.5f);
 
         }
 
         //one hit
-        if (barrierState==2) 
+        if (barrierState == 2)
         {
             myRendere.material.SetFloat("_Enabledistortion", 0.4f - mat.GetFloat("_Enabledistortion"));
-            myRendere.material.SetFloat("_Globalopacity",0.6f);
-             myRendere.material.SetColor("_Maincolor", Color.red);
-             myRendere.material.SetFloat("_Distortionspeed",1f);
+            myRendere.material.SetFloat("_Globalopacity", 0.6f);
+            myRendere.material.SetColor("_Maincolor", Color.red);
+            myRendere.material.SetFloat("_Distortionspeed", 1f);
 
         }
 
         //almost destroyed
-         if (barrierState==3) 
+        if (barrierState == 3)
         {
             myRendere.material.SetFloat("_Enabledistortion", 0.8f - mat.GetFloat("_Enabledistortion"));
-            myRendere.material.SetFloat("_Globalopacity",0.5f);
-            myRendere.material.SetFloat("_Distortionspeed",2f);
+            myRendere.material.SetFloat("_Globalopacity", 0.5f);
+            myRendere.material.SetFloat("_Distortionspeed", 2f);
         }
 
 
     }
-  
+
 }
