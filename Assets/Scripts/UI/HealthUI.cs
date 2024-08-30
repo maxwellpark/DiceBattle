ce03ab2e2528b1de93fe2e558a58d4333ea97675
Max <61809127+maxwellpark@users.noreply.github.com>
2024-08-30 14:56:31 +0100

    Integrate health bars

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
@@ -16,6 +17,19 @@ public class HealthUI : MonoBehaviour
     private string _p1Prefix = "P1 health: ";
     [SerializeField]
     private string _p2Prefix = "P2 health: ";
+
+    [SerializeField]
+    private Image _p1Image;
+    [SerializeField]
+    private Image _p2Image;
+    [SerializeField]
+    private Sprite _3HpSprite;
+    [SerializeField]
+    private Sprite _2HpSprite;
+    [SerializeField]
+    private Sprite _1HpSprite;
+    [SerializeField]
+    private Sprite _0HpSprite;
 
     private (UnityAction<int> p1, UnityAction<int> p2) _dmgActions;
     private (UnityAction p1, UnityAction p2) _rdActions;
@@ -44,19 +58,33 @@ public class HealthUI : MonoBehaviour
     {
         var (p1, p2) = GetPlayerShootingComps();
 
-        UnityAction<int> p1DmgAction = h => SetText(h, _p1HealthText, _p1Prefix);
-        UnityAction<int> p2DmgAction = h => SetText(h, _p2HealthText, _p2Prefix);
+        //UnityAction<int> p1DmgAction = h => SetText(h, _p1HealthText, _p1Prefix);
+        //UnityAction<int> p2DmgAction = h => SetText(h, _p2HealthText, _p2Prefix);
+        void p1DmgAction(int h) => SetSprite(h, _p1Image);
+        void p2DmgAction(int h) => SetSprite(h, _p2Image);
         p1.onDamageTaken += p1DmgAction;
         p2.onDamageTaken += p2DmgAction;
         _dmgActions = (p1DmgAction, p2DmgAction);
 
-        UnityAction p1RdAction = () => SetText(p1.health, _p1HealthText, _p1Prefix);
-        UnityAction p2RdAction = () => SetText(p2.health, _p2HealthText, _p2Prefix);
+        //UnityAction p1RdAction = () => SetText(p1.health, _p1HealthText, _p1Prefix);
+        //UnityAction p2RdAction = () => SetText(p2.health, _p2HealthText, _p2Prefix);
+        void p1RdAction()
+        {
+            _p1Image.gameObject.SetActive(true);
+            SetSprite(p1.health, _p1Image);
+        }
+
+        void p2RdAction()
+        {
+            _p2Image.gameObject.SetActive(true);
+            SetSprite(p2.health, _p2Image);
+        }
+
         GameManager.onNewRound += p1RdAction;
         GameManager.onNewRound += p2RdAction;
         _rdActions = (p1RdAction, p2RdAction);
 
-        SetActive(true);
+        SetTextsActive(true);
     }
 
     private void TearDown()
@@ -73,21 +101,47 @@ public class HealthUI : MonoBehaviour
         GameManager.onNewRound -= _rdActions.p2;
     }
 
-    public void SetActive(bool active)
+    public void SetTextsActive(bool active)
     {
         _p1HealthText.gameObject.SetActive(active);
         _p2HealthText.gameObject.SetActive(active);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        SetActive(false);
+        SetTextsActive(false);
+        _p1Image.gameObject.SetActive(false);
+        _p2Image.gameObject.SetActive(false);
     }
 
     private void SetText(int health, TMP_Text txt, string prefix)
     {
         txt.text = prefix + health;
+    }
+
+    private void SetSprite(int health, Image image)
+    {
+        if (health > 3 || health < 0)
+        {
+            Debug.LogError("Unexpected health: " + health);
+            return;
+        }
+
+        switch (health)
+        {
+            case 3:
+                image.sprite = _3HpSprite;
+                break;
+            case 2:
+                image.sprite = _2HpSprite;
+                break;
+            case 1:
+                image.sprite = _1HpSprite;
+                break;
+            case 0:
+                image.sprite = _0HpSprite;
+                break;
+        }
     }
 
     private void OnDestroy()
