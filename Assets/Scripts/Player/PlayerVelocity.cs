394cdd15991e8ff2a68697955c8f7fedd630bd17
j <72587410+akimkalsky@users.noreply.github.com>
2024-08-31 18:40:01 -0300

    sloppy code
    do wonder if now it's just the same as input pressing...
    well I've changed cause when moving z or x was alwayer changing
    even if twaas a small value the animator ended up haveing 2 or 3
    state to be true...

--- Assets/Scripts/Player/PlayerVelocity.cs
+++ Assets/Scripts/Player/PlayerVelocity.cs
@@ -5,6 +5,8 @@ public class PlayerVelocity : MonoBehaviour
     private Vector3 lastPosition;
     public float velocityX;
     public float velocityZ;
+    public float speedNullifierZ=1;
+    public float speedNullifierX = 1;
 
     void Start()
     {
@@ -15,13 +17,31 @@ public class PlayerVelocity : MonoBehaviour
     void Update()
     {
         // Calculate the velocity for each axis separately
-        velocityX = (transform.position.x - lastPosition.x) / Time.deltaTime;
-        velocityZ = (transform.position.z - lastPosition.z) / Time.deltaTime;
+        velocityX =speedNullifierX * (transform.position.x - lastPosition.x) / Time.deltaTime;
+        velocityZ = speedNullifierZ * (transform.position.z - lastPosition.z) / Time.deltaTime;
 
         // Update lastPosition for the next frame
         lastPosition = transform.position;
 
         // Optional: Debug the velocity
-        Debug.Log("Player Velocity - X: " + velocityX + ", Z: " + velocityZ);
+        // Debug.Log("Player Velocity - X: " + velocityX + ", Z: " + velocityZ);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+
+            speedNullifierZ = 0;
+            speedNullifierX = 1;
+
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+
+            speedNullifierZ = 1;
+            speedNullifierX = 0;
+
+        }
+
+
     }
 }
